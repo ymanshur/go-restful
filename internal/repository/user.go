0327a77eb3743a007a503d276a6637565e9e1ff9
Yusuf Manshur <ymanshur@gmail.com>
2022-10-10 11:40:38 +0700

    Added User repository factory interface

--- internal/repository/user.go
+++ internal/repository/user.go
@@ -1,79 +1,85 @@
 package repository
 
 import (
+	"go-restful/internal/app/dto"
 	"go-restful/internal/model"
-	"go-restful/internal/pkg/util"
 
 	"gorm.io/gorm"
 )
 
-type User struct {
-	db *gorm.DB
-}
-
-func (u *User) Save(user *model.User) (*model.User, error) {
-	if err := u.db.Save(&user).Error; err != nil {
-		return nil, err
+type (
+	User interface {
+		Save(payload *dto.CreateUserRequest) error
+		FindById(userId uint) (*model.User, error)
+		UpdateById(userId uint, user *model.User) (*model.User, error)
+		DeleteById(userId uint) error
+		FindAll() ([]*model.User, error)
+		FindByEmail(email *string) (*model.User, error)
 	}
-	return user, nil
+	userRepo struct {
+		db *gorm.DB
+	}
+)
+
+func NewUser(db *gorm.DB) User {
+	return &userRepo{
+		db,
+	}
 }
 
-func (u *User) FindById(userId uint) (*model.User, error) {
+func (u *userRepo) Save(payload *dto.CreateUserRequest) error {
+	newUser := model.User{
+		Name:     payload.Name,
+		Email:    payload.Email,
+		Password: payload.Password,
+	}
+	if err := u.db.Save(&newUser).Error; err != nil {
+		return err
+	}
+	return nil
+}
+
+func (u *userRepo) FindById(userId uint) (*model.User, error) {
 	user := new(model.User)
-	if err := u.db.Omit("token", "password").Where(model.User{ID: userId}).Take(&user).Error; err != nil {
+	if err := u.db.Omit("password").Where(model.User{ID: userId}).Take(&user).Error; err != nil {
 		return nil, err
 	}
 	return user, nil
 }
 
-func (u *User) UpdateById(userId uint, user *model.User) (*model.User, error) {
+func (u *userRepo) UpdateById(userId uint, user *model.User) (*model.User, error) {
 	if err := u.db.Model(&model.User{ID: userId}).Updates(&user).Error; err != nil {
 		return nil, err
 	}
 
 	updatedUser := new(model.User)
-	if err := u.db.Omit("token", "password").Find(&updatedUser, userId).Error; err != nil {
+	if err := u.db.Omit("password").Find(&updatedUser, userId).Error; err != nil {
 		return nil, err
 	}
 	return updatedUser, nil
 }
 
-func (u *User) DeleteById(userId uint) error {
+func (u *userRepo) DeleteById(userId uint) error {
 	if err := u.db.Delete(&model.User{}, userId).Error; err != nil {
 		return err
 	}
 	return nil
 }
 
-func (u *User) FindAll() ([]*model.User, error) {
+func (u *userRepo) FindAll() ([]*model.User, error) {
 	var users []*model.User
-	if err := u.db.Omit("token", "password").Find(&users).Error; err != nil {
+	if err := u.db.Omit("password").Find(&users).Error; err != nil {
 		return nil, err
 	}
 	return users, nil
 }
 
-func (u *User) FindByEmailPassword(user *model.User) (*model.User, error) {
-	if err := u.db.Where(model.User{Email: user.Email, Password: user.Password}).Take(&user).Error; err != nil {
+func (u *userRepo) FindByEmail(email *string) (*model.User, error) {
+	user := new(model.User)
+	if err := u.db.
+		Omit("password").Where("email = ?", email).
+		Take(&user).Error; err != nil {
 		return nil, err
 	}
-
-	//-----
-	// JWT
-	//-----
-
-	// Create token
-	token, err := util.CreateJwt(user)
-	if err != nil {
-		return nil, err
-	}
-
-	user.Token = token
 	return user, nil
-}
-
-func NewUserRepository(db *gorm.DB) *User {
-	return &User{
-		db,
-	}
 }
